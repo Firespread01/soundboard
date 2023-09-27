4e1a2b1d8bb332013683be84dd777eb4fc64901c
fire <102771014+Firespread01@users.noreply.github.com>
2023-09-27 10:11:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function playAudio(url) {
+    new Audio(url).play();  
+}
+
+function stopAudio() {
+  location.reload()
+}
+
+function funny() {
+  for(var i = 0; i < 10; i++) {
+    playAudio('Sounds/spider-man-2099.mp3');
+    playAudio('Sounds/vine-boom.mp3');
+    playAudio('Sounds/oh-my-god-vine.mp3');
+    playAudio('Sounds/taco-bell-bong.mp3');
+    playAudio('Sounds/baby-laughing-meme.mp3');
+    }
+}
